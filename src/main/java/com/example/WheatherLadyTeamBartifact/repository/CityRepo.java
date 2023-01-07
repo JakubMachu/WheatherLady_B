f999cc0cd150bcc653daaef52d9411405f286e68
JakubMachu <jacubmachu@gmail.com>
2023-01-07 11:33:56 +0100

    Long to UUID change

--- src/main/java/com/example/WheatherLadyTeamBartifact/repository/CityRepo.java
+++ src/main/java/com/example/WheatherLadyTeamBartifact/repository/CityRepo.java
@@ -2,5 +2,7 @@ package com.example.WheatherLadyTeamBartifact.repository;
 import com.example.WheatherLadyTeamBartifact.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CityRepo extends JpaRepository <City, Long> {
+import java.util.UUID;
+
+public interface CityRepo extends JpaRepository <City, UUID> {
 }
